b54afea6c7194004b0b43d99414fc0e0def7a6b2
alex <win32coder@list.ru>
2020-07-23 11:32:25 +0300

    Add project files.

---
+++ BotReply/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BotReply
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
